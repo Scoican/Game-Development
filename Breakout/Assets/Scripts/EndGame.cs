46d1a8c654a09ac3152cfd4bae0196c92944e08a
Scoican Ovidiu <scoican.ovidiu@gmail.com>
2019-10-20 02:37:33 +0300

    Game Losing

---
+++ Breakout/Assets/Scripts/EndGame.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndGame : MonoBehaviour
+{
+    public Text endGameText;
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.name == "Ball")
+        {
+            endGameText.enabled = true;
+            Time.timeScale = 0;
+        }
+    }
+}
