998b76aaf2246f64e4c1426180f4fb80255fcba2
Scoican Ovidiu <scoican.ovidiu@gmail.com>
2020-06-13 22:02:42 +0300

    Changes

---
+++ Other Games/Tower Defense/Assets/Scripts/SceneFaderScript.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SceneFaderScript : MonoBehaviour {
+
+    public Image image;
+    public AnimationCurve fade;
+
+    void Start()
+    {
+        StartCoroutine(FadeIn());   
+    }
+
+    public void FadeTo(string sceneName)
+    {
+            StartCoroutine(FadeOut(sceneName));
+    }
+
+    IEnumerator FadeIn()
+    {
+        float time = 1f;
+        while (time > 0f)
+        {
+            time -= Time.deltaTime;
+            float alpha = fade.Evaluate(time);
+            image.color = new Color(0f, 0f, 0f,alpha);
+            yield return 0;
+        } 
+    }
+
+    IEnumerator FadeOut(string sceneName)
+    {
+        float time = 0f;
+        while (time < 1f)
+        {
+            time += Time.deltaTime;
+            float alpha = fade.Evaluate(time);
+            image.color = new Color(0f, 0f, 0f, alpha);
+            yield return 0;
+        }
+        SceneManager.LoadScene(sceneName);
+    }
+}
---
+++ Other Games/Tower Defense/Assets/Scripts/TurretUIScript.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TurretUIScript : MonoBehaviour {
+
+    private NodeScript target;
+    public GameObject canvasUI;
+
+    public Text upgradeCost;
+    public Button upgradeButton;
+
+    public Text sellCost;
+
+    public void SetTarget(NodeScript node)
+    {
+        this.target = node;
+        transform.position = target.GetBuildPosition();
+        if (!target.isUpgraded)
+        {
+            upgradeCost.text = "$" + target.turretBlueprint.cost;
+            upgradeButton.interactable = true;
+        }
+        else
+        {
+            upgradeCost.text = "UNAVAILABLE";
+            upgradeButton.interactable = false;
+        }
+        sellCost.text = "$" + target.turretBlueprint.GetSellCost();
+        canvasUI.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        canvasUI.SetActive(false);
+    }
+
+    public void Upgrade()
+    {
+        if (target.isUpgraded)
+        {
+            return;
+        }
+        target.UpgradeTurret();
+        BuildManager.instance.DeselectNode();
+    }
+
+    public void Sell()
+    {
+        target.SellTurret();
+        BuildManager.instance.DeselectNode();
+    }
+}
